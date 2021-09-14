f097400c54c773049794efd7631e9c4264e163aa
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-09-14 08:15:08 +0100

    Finish Home Screen

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import tw from 'tailwind-react-native-classnames'
 
 const data=[
 {
@@ -24,12 +26,32 @@ const NavOptions = () => {
        keyExtractor={(item)=> item.id}
         data={data}
         horizontal
-        renderItem={({item})=>{
-         <TouchableOpacity  >
-             <Text >{item.title}</Text>
+        renderItem={({item})=>(
+         <TouchableOpacity  
+         style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+         >
+            <View>
+                <Image 
+                source={{
+                    uri:item.image
+                }}
+                style={{
+                    width:120 , height: 120,
+                    resizeMode:"contain"
+                }}
+                />
+                <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                 <Icon 
+                 style={tw`p-2 bg-black rounded-full w-10 mt-4 `}
+                 type="antdesign"
+                 color="white"
+                 name="arrowright"
+                 />
+         
+            </View>
          </TouchableOpacity>
 
-        }}
+        )}
        
        />
     )
